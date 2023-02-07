3da3976be03f620b6678acb56406476d318520be
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-02-07 14:33:35 +0300

    Create AutomatedWebScraping.py

---
+++ AutomatedWebScraping.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+# navigate to the web application
+driver.get("https://www.example.com")
+
+# find the search bar element
+search_bar = driver.find_element_by_xpath("//input[@name='q']")
+
+# enter the keyword you want to search for
+search_bar.send_keys("keyword")
+
+# submit the form
+search_bar.submit()
+
+# wait for the results to load
+time.sleep(5)
+
+# find all the results on the page
+results = driver.find_elements_by_xpath("//div[@class='result']")
+
+# loop through the results and print the text of each result
+for result in results:
+    print(result.text)
+
+# press the 'Page Down' key to load the next page of results
+search_bar.send_keys(Keys.PAGE_DOWN)
+
+# wait for the next page of results to load
+time.sleep(5)
+
+# find all the results on the second page
+results = driver.find_elements_by_xpath("//div[@class='result']")
+
+# loop through the results and print the text of each result
+for result in results:
+    print(result.text)
+
+# close the browser
+driver.close()
