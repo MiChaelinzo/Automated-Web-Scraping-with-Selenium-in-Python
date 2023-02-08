e7b4dc02d03d50526083c6e98955354442a1590c
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-02-08 13:54:12 +0300

    Update AutomatedWebScraping2.py

--- AutomatedWebScraping2.py
+++ AutomatedWebScraping2.py
@@ -8,32 +8,34 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Firefox()
 
 # Navigate to the target website
-driver.get("https://www.Google.com")
+driver.get("https://www.google.com")
 
 # Find the search bar
-search_bar = driver.find_element_by_xpath("//input[@id='search-bar']")
+search_bar = driver.find_element(by=By.NAME, value="q")
 
 # Enter the search keyword
-search_bar.send_keys("keyword")
+search_bar.send_keys("DragonBall")
 search_bar.send_keys(Keys.RETURN)
 
 # Wait for the results to load
 wait = WebDriverWait(driver, 10)
-results = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='result']")))
+results = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='g']")))
 
 # Extract the information from the results
 for result in results:
     # Extract the title
-    title = result.find_element_by_xpath(".//h3[@class='title']").text
+    title = result.find_element(by=By.XPATH, value=".//h3").text
 
     # Extract the description
-    description = result.find_element_by_xpath(".//p[@class='description']").text
+    description = result.find_element(by=By.XPATH, value=".//span[@class='st']").text
 
     # Extract the link
-    link = result.find_element_by_xpath(".//a[@class='link']").get_attribute("href")
+    link = result.find_element(by=By.XPATH, value=".//a").get_attribute("href")
 
     # Do something with the extracted information (e.g. print it, write it to a file, etc.)
     print(f"Title: {title}\nDescription: {description}\nLink: {link}\n")
 
 # Close the web driver
 driver.quit()
+
+
