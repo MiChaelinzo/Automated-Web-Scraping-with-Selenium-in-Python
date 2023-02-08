21ea5edcc374984aa1441b3e4e9b1edc487fd503
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-02-08 23:12:05 +0300

    Update AutomatedWebScraping.py

--- AutomatedWebScraping.py
+++ AutomatedWebScraping.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 # create a new instance of the Chrome driver
