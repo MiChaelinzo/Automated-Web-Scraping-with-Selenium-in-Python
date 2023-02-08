771298591c046c271a83b9b05dc1bc2666f4483e
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-02-08 23:25:59 +0300

    Update AutomatedWebScraping2.py

--- AutomatedWebScraping2.py
+++ AutomatedWebScraping2.py
@@ -11,7 +11,7 @@ driver = webdriver.Firefox()
 driver.get("https://www.google.com")
 
 # Find the search bar
-search_bar = driver.find_element(by=By.NAME, value="q")
+search_bar = driver.find_element(By.NAME, value="q")
 
 # Enter the search keyword
 search_bar.send_keys("DragonBall")
@@ -24,13 +24,13 @@ results = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@clas
 # Extract the information from the results
 for result in results:
     # Extract the title
-    title = result.find_element(by=By.XPATH, value=".//h3").text
+    title = result.find_element(By.XPATH, value=".//h3").text
 
     # Extract the description
     description = result.find_element(by=By.XPATH, value=".//span[@class='st']").text
 
     # Extract the link
-    link = result.find_element(by=By.XPATH, value=".//a").get_attribute("href")
+    link = result.find_element(By.XPATH, value=".//a").get_attribute("href")
 
     # Do something with the extracted information (e.g. print it, write it to a file, etc.)
     print(f"Title: {title}\nDescription: {description}\nLink: {link}\n")
